a194d889c7ca04f583c634341fac80c3787122d7
mardon1 <mmardon540@gmail.com>
2021-11-16 16:29:21 +0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const email = document.getElementById("email");
+const icon1 = document.querySelector(".icon1");
+const icon2 = document.querySelector(".icon2");
+const errorText = document.querySelector(".error-text");
+const btn = document.querySelector("button");
+let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+email.addEventListener("keyup", function () {
+  check();
+});
+
+function check() {
+  if (email.value.match(regExp)) {
+    email.style.borderColor = "#27ae60";
+    icon1.style.display = "none";
+    icon2.style.display = "block";
+    errorText.style.display = "none";
+    btn.style.display = "block";
+  } else {
+    email.style.borderColor = "#e74c3c";
+    icon1.style.display = "block";
+    icon2.style.display = "none";
+    errorText.style.display = "block";
+    btn.style.display = "none";
+  }
+
+  if (email.value === "") {
+    email.style.borderColor = "lightgrey";
+    icon1.style.display = "none";
+    icon2.style.display = "none";
+    errorText.style.display = "none";
+    btn.style.display = "none";
+  }
+}
